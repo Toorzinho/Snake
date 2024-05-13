151cacf32712cce54782fcf7390be140ce4d88b3
ADesperateAttempt <146085981+ADesperateAttempt@users.noreply.github.com>
2024-05-13 10:58:07 +0200

    4. Commit (Bevægelse)

--- main.py
+++ main.py
@@ -29,4 +29,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != 'DOWN':
+                direction = 'UP'
+            elif event.key == pygame.K_DOWN and direction != 'UP':
+                direction = 'DOWN'
+            elif event.key == pygame.K_LEFT and direction != 'RIGHT':
+                direction = 'LEFT'
+            elif event.key == pygame.K_RIGHT and direction != 'LEFT':
+                direction = 'RIGHT'
+                
+     # Move the snake
+    if direction == 'UP':
+        snake_pos[0] = (snake_pos[0][0], snake_pos[0][1] - speed)
+    elif direction == 'DOWN':
+        snake_pos[0] = (snake_pos[0][0], snake_pos[0][1] + speed)
+    elif direction == 'LEFT':
+        snake_pos[0] = (snake_pos[0][0] - speed, snake_pos[0][1])
+    elif direction == 'RIGHT':
+        snake_pos[0] = (snake_pos[0][0] + speed, snake_pos[0][1])
